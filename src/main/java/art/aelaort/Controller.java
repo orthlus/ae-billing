b906ea71ab38ef3bbfce74952424c6c527d6da0d
ort <orthodoxplus@ya.ru>
2024-09-07 19:31:29 +0300

    lombok

--- src/main/java/art/aelaort/Controller.java
+++ src/main/java/art/aelaort/Controller.java
@@ -1,17 +1,15 @@
 package art.aelaort;
 
 import art.aelaort.billing.BillingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class Controller {
 	private final BillingService billingService;
-
-	public Controller(BillingService billingService) {
-		this.billingService = billingService;
-	}
 
 	@GetMapping("/billing/{serviceName}")
 	public String billing(@PathVariable(required = false) String serviceName) {
