b19833a239ad19d0173173eae28c02d7076a126a
ort <orthodoxplus@ya.ru>
2024-11-21 02:27:39 +0300

    update ya-iam-helper

--- src/main/java/art/aelaort/billing/BillingProperties.java
+++ src/main/java/art/aelaort/billing/BillingProperties.java
@@ -5,8 +5,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
-
 @Getter
 @Setter
 @Component
@@ -19,26 +17,5 @@ public class BillingProperties {
 	private String tiktokUrl;
 	private String tiktokToken;
 	private String yandexUrl;
-	private YandexIAMS3 yandexIAMS3;
-	private YandexSecrets yandexSecrets;
-	private String sberStaticString;
 	private String pqStaticString;
-
-	@Getter
-	@Setter
-	public static class YandexIAMS3 {
-		private String url;
-		private String region;
-		private String id;
-		private String key;
-		private String bucket;
-		private String file;
-	}
-
-	@Getter
-	@Setter
-	public static class YandexSecrets {
-		private URI url;
-		private String secret;
-	}
 }
--- src/main/java/art/aelaort/billing/yandex/YandexConfig.java
+++ src/main/java/art/aelaort/billing/yandex/YandexConfig.java
@@ -2,26 +2,16 @@ package art.aelaort.billing.yandex;
 
 import art.aelaort.YandexIAMConfig;
 import art.aelaort.YandexIAMSupplier;
-import art.aelaort.billing.BillingProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@EnableConfigurationProperties(YandexIAMConfig.class)
 @Configuration
 public class YandexConfig {
 	@Bean
-	public YandexIAMSupplier yaIAMSupplier(RestTemplate yandexRestTemplate,
-										   BillingProperties billingProperties) {
-		return new YandexIAMSupplier(yandexRestTemplate,
-				new YandexIAMConfig(
-						billingProperties.getYandexIAMS3().getId(),
-						billingProperties.getYandexIAMS3().getKey(),
-						billingProperties.getYandexIAMS3().getUrl(),
-						billingProperties.getYandexIAMS3().getRegion(),
-						billingProperties.getYandexIAMS3().getFile(),
-						billingProperties.getYandexIAMS3().getBucket(),
-						billingProperties.getYandexSecrets().getSecret(),
-						billingProperties.getYandexSecrets().getUrl()
-				));
+	public YandexIAMSupplier yaIAMSupplier(RestTemplate yandexRestTemplate, YandexIAMConfig yandexIAMConfig) {
+		return new YandexIAMSupplier(yandexRestTemplate, yandexIAMConfig);
 	}
 }
